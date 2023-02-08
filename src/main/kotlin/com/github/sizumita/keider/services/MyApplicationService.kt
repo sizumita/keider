870be3a49921eb860afdee5499d3880d36b41f7d
GitHub Action <action@github.com>
2023-02-08 09:08:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/sizumita/keider/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sizumita/keider/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sizumita.keider.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sizumita.keider.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/sizumita/keider/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sizumita/keider/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sizumita.keider.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sizumita.keider.MyBundle
 
 class MyProjectService(project: Project) {
 
